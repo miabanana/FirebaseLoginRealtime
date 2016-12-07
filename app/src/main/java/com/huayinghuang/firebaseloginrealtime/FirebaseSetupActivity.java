35aa35ad6bc47350b87cd82804fcbfc75277d974
Hua Ying Huang <b00107012@ntu.edu.tw>
2016-12-07 11:13:17 +0800

    realtime database - add online user numbers

--- app/src/main/java/com/huayinghuang/firebaseloginrealtime/FirebaseSetupActivity.java
+++ app/src/main/java/com/huayinghuang/firebaseloginrealtime/FirebaseSetupActivity.java
@@ -1,14 +1,21 @@
 package com.huayinghuang.firebaseloginrealtime;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by miahuang on 2016/11/22.
@@ -16,14 +23,37 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class FirebaseSetupActivity extends AppCompatActivity {
     protected static final String TAG = "Test";
+    protected static final String DB_ONLINE_USER = "online_user";
+    protected static final String DB_NUM = "num";
+    private static final String DB_USER_LIST = "users";
+    private static final String DB_MAIL = "mail";
+    private static final String DB_STATUS = "is_login";
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseUser mUser;
+    private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+
+    private Handler mHandler;
+
+    protected void updateNumbers(final TextView textView) {
+        mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if(msg.what == 0) {
+                    if (textView != null) {
+                        textView.setText(String.format(getString(R.string.online), (int) msg.obj));
+                    }
+                }
+            }
+        };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        updateNumbers(null);
 
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -32,6 +62,8 @@ public class FirebaseSetupActivity extends AppCompatActivity {
                 mUser = firebaseAuth.getCurrentUser();
                 if (mUser != null) {
                     Log.d(TAG,"onAuthStateChanged:signed_in: " + mUser.getUid());
+                    writeDatabase(true, 1);
+                    readDatabase();
                 } else {
                     Log.d(TAG,"onAuthStateChanged:signed_out");
                 }
@@ -53,12 +85,6 @@ public class FirebaseSetupActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        signOutFirebase();
-    }
-
     public FirebaseAuth getFirebaseAuth() {
         return mAuth;
     }
@@ -67,19 +93,60 @@ public class FirebaseSetupActivity extends AppCompatActivity {
         return mUser;
     }
 
+    public FirebaseDatabase getFirebaseDB() {
+        return mDatabase;
+    }
+
     public boolean isFirebaseUserNull() {
         return mUser == null;
     }
 
-    private void writeDatabase(int value) {
-        String DB_ONLINE_USER = "online_user/num";
-        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference reference = mDatabase.getReference(DB_ONLINE_USER);
-        reference.setValue(value);
+    private void writeDatabase(boolean isLogin, int value) {
+        if (!isFirebaseUserNull()) {
+            Log.d(TAG, "writeDatabase");
+            //no need to update when logout
+            if (isLogin == true) {
+                mDatabase.getReference(DB_USER_LIST)
+                        .child(getFirebaseUser().getUid())
+                        .child(DB_MAIL)
+                        .setValue(UserData.getInstance().getMail());
+            }
+            mDatabase.getReference(DB_USER_LIST)
+                    .child(getFirebaseUser().getUid())
+                    .child(DB_STATUS)
+                    .setValue(isLogin);
+            mDatabase.getReference(DB_ONLINE_USER)
+                    .child(DB_NUM)
+                    .setValue(value);
+        }
+    }
+
+    private void readDatabase() {
+        if (!isFirebaseUserNull()) {
+            Log.d(TAG, "readDatabase");
+            DatabaseReference reference = mDatabase.getReference(DB_ONLINE_USER).child(DB_NUM);
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    final int num = dataSnapshot.getValue(Integer.class);
+                    Log.d(TAG, "Realtime DB changed value = " + num);
+                    final Message message = mHandler.obtainMessage(0,num);
+//                    message.what = 0;
+//                    message.arg1 = num;
+                    message.sendToTarget();
+//                myHandler.sendMessage(message);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.w(TAG, "Failed to read value: " + databaseError);
+                }
+            });
+        }
     }
 
     public void signOutFirebase() {
-        writeDatabase(-1);
+        writeDatabase(false, -1);
         UserData.getInstance().clearAll();
         mAuth.signOut();
     }
--- app/src/main/java/com/huayinghuang/firebaseloginrealtime/RealtimeDBActivity.java
+++ app/src/main/java/com/huayinghuang/firebaseloginrealtime/RealtimeDBActivity.java
@@ -10,22 +10,21 @@ import android.widget.Toast;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class RealtimeDBActivity extends FirebaseSetupActivity implements View.OnClickListener {
-    private static final String TAG = "Test";
-    private static final String DB_ONLINE_USER = "online_user";
-    private static final String DB_NUM = "num";
-    private static final String DB_USER_LIST = "users";
-    private static final String DB_MAIL = "mail";
-
-    private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+public class RealtimeDBActivity extends FirebaseSetupActivity {
     private long mFirstClick = 0;
     private TextView mOnlineUser;
+
+//    public Handler mHandler = new Handler(Looper.getMainLooper()) {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//            if (msg.what == 0) {
+//                mOnlineUser.setText(String.format(getString(R.string.online), (int)msg.obj));
+//            }
+//        }
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +36,8 @@ public class RealtimeDBActivity extends FirebaseSetupActivity implements View.On
     protected void onResume() {
         super.onResume();
         initView();
-        updateDB();
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.signout:
-                signOutFirebase();
-                finish();
-                break;
-            default:
-        }
+        updateNumbers(mOnlineUser);
+//        updateOnlineUserNum();
     }
 
     @Override
@@ -57,6 +46,7 @@ public class RealtimeDBActivity extends FirebaseSetupActivity implements View.On
             Toast.makeText(this, R.string.exit, Toast.LENGTH_SHORT).show();
             mFirstClick = System.currentTimeMillis();
         } else {
+            signOutFirebase();
             finish();
         }
     }
@@ -69,51 +59,26 @@ public class RealtimeDBActivity extends FirebaseSetupActivity implements View.On
         user.setText(String.format(getString(R.string.user), UserData.getInstance().getDisplayName()));
 
         mOnlineUser = (TextView)findViewById(R.id.onlineUsers);
-        mOnlineUser.setText(String.format(getString(R.string.online), 1));
+        mOnlineUser.setText(String.format(getString(R.string.online), 0));
 
-        findViewById(R.id.signout).setOnClickListener(this);
+        findViewById(R.id.signout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOutFirebase();
+                finish();
+            }
+        });
     }
 
-    private void updateDB() {
-        Log.d(TAG, "updateDB");
-        if (!isFirebaseUserNull()) {
-            writeDatabase(1);
-            readDatabase();
-        }
-    }
-
-    private void writeDatabase(int value) {
-        Log.d(TAG, "writeDatabase");
-        Map<String, String> map = new HashMap<>();
-        map.put(DB_MAIL, UserData.getInstance().getMail());
-        mDatabase.getReference(DB_USER_LIST+"/"+getFirebaseUser().getUid())
-                .setValue(map);
-
-//        Map<String, Integer> map1 = new HashMap<>();
-//        map1.put(DB_NUM, value);
-//        mDatabase.getReference(DB_ONLINE_USER).setValue(map1);
-        mDatabase.getReference(DB_ONLINE_USER+"/"+DB_NUM).setValue(value);
-    }
-
-    private void readDatabase() {
-        Log.d(TAG, "readDatabase");
-//        DatabaseReference reference = mDatabase.getReference(DB_ONLINE_USER);
-        DatabaseReference reference = mDatabase.getReference(DB_ONLINE_USER+"/"+DB_NUM);
+    private void updateOnlineUserNum() {
+        Log.d(TAG, "update online user numbers");
+        DatabaseReference reference = getFirebaseDB().getReference(DB_ONLINE_USER).child(DB_NUM);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-//                GenericTypeIndicator<HashMap<String,Integer>> indicator = new GenericTypeIndicator<HashMap<String, Integer>>() {};
-//                HashMap map = dataSnapshot.getValue(indicator);
-//                final int num1 = (int)map.get(DB_NUM);
-
                 final int num = dataSnapshot.getValue(Integer.class);
                 Log.d(TAG,"Realtime DB changed value = " + num);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mOnlineUser.setText(String.format(getString(R.string.online), num));
-                    }
-                });
+                mOnlineUser.setText(String.format(getString(R.string.online), num));
             }
 
             @Override
